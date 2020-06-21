75a8ef4bcfc7737b3b2bce6e3fa56e27c7b076bc
BuildTools <unconfigured@null.spigotmc.org>
2020-06-21 19:45:55 +0200

    sound

--- src/app/slot/slot.component.ts
+++ src/app/slot/slot.component.ts
@@ -7,16 +7,16 @@ import { Component, OnInit } from "@angular/core";
 })
 export class SlotComponent implements OnInit {
   symbols: any = [
-    { value: 0, symbol: "../../assets/images/symbols/Asset1.svg" },
-    { value: 1, symbol: "../../assets/images/symbols/Asset11.svg" },
-    { value: 2, symbol: "../../assets/images/symbols/Asset3.svg" },
-    { value: 3, symbol: "../../assets/images/symbols/Asset4.svg" },
-    { value: 4, symbol: "../../assets/images/symbols/Asset5.svg" },
-    { value: 5, symbol: "../../assets/images/symbols/Asset6.svg" },
-    { value: 6, symbol: "../../assets/images/symbols/Asset7.svg" },
-    { value: 7, symbol: "../../assets/images/symbols/Asset8.svg" },
-    { value: 8, symbol: "../../assets/images/symbols/Asset9.svg" },
-    { value: 9, symbol: "../../assets/images/symbols/Asset10.svg" },
+    { value: 2, symbol: "../../assets/images/symbols/Asset1.svg" },
+    { value: 3, symbol: "../../assets/images/symbols/Asset11.svg" },
+    { value: 4, symbol: "../../assets/images/symbols/Asset3.svg" },
+    { value: 5, symbol: "../../assets/images/symbols/Asset4.svg" },
+    { value: 6, symbol: "../../assets/images/symbols/Asset5.svg" },
+    { value: 7, symbol: "../../assets/images/symbols/Asset6.svg" },
+    { value: 8, symbol: "../../assets/images/symbols/Asset7.svg" },
+    { value: 9, symbol: "../../assets/images/symbols/Asset8.svg" },
+    { value: 10, symbol: "../../assets/images/symbols/Asset9.svg" },
+    { value: 11, symbol: "../../assets/images/symbols/Asset10.svg" },
   ];
 
   SymbolHolder = [
@@ -43,17 +43,23 @@ export class SlotComponent implements OnInit {
   ];
   savedSymbols = [];
   activeSpin = false;
-
+  currentSymbol = [];
   // LINES
 
-  _line = [];
-  constructor() {}
+  line1 = [
+    {
+      row1: [1, 1, 0, 0, 0]
+    }
+  ];
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   spin() {
+    const spinAudio = document.getElementById('audio') as any;
+    spinAudio.play();
     for (let i = 0; i < 20; i++) {
-      if (this.SymbolHolder != undefined) {
+      if (this.SymbolHolder !== undefined) {
         this.savedSymbols.push(this.SymbolHolder[i]);
       }
     }
@@ -64,7 +70,13 @@ export class SlotComponent implements OnInit {
       r = Math.floor(r);
       this.SymbolHolder.push(this.symbols[r]);
     }
-    this.checkWin(this.savedSymbols);
+    for (let i = 0; i < 20; i++) {
+      if (this.SymbolHolder !== undefined) {
+        this.currentSymbol.push(this.SymbolHolder[i]);
+      }
+    }
+    this.checkWin(this.currentSymbol);
+    this.currentSymbol = [];
     this.SymbolHolder = this.SymbolHolder.concat(this.savedSymbols);
     this.savedSymbols = [];
     setTimeout(() => {
@@ -74,6 +86,7 @@ export class SlotComponent implements OnInit {
 
   checkWin(symbols) {
     console.log(symbols);
+    // Not working yet. So you can't win :D
   }
-  disableSpin() {}
+
 }
